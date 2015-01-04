990e099b5644bdcdf08abc44306df854fb088a73
Geonmo <Geonmo@Geonmo-PC>
2015-01-04 20:30:32 +0900

    XY플롯 정리

--- src/YoungDoubleSlit/YoungDoubleSlit.java
+++ src/YoungDoubleSlit/YoungDoubleSlit.java
@@ -18,26 +18,14 @@ import java.util.ArrayList;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.axis.CategoryLabelPositions;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.CategoryItemLabelGenerator;
-import org.jfree.chart.labels.ItemLabelAnchor;
-import org.jfree.chart.labels.ItemLabelPosition;
-import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.DatasetRenderingOrder;
-import org.jfree.chart.plot.PlotOrientation;
-//import org.jfree.chart.renderer.category.BarRenderer;
-//import org.jfree.chart.renderer.category.CategoryItemRenderer;
-import org.jfree.chart.renderer.category.LineAndShapeRenderer;
-//import org.jfree.chart.renderer.category.StandardBarPainter;
-//import org.jfree.chart.title.TextTitle;
-import org.jfree.data.category.DefaultCategoryDataset;
-//import org.jfree.ui.GradientPaintTransformType;
-//import org.jfree.ui.HorizontalAlignment;
-//import org.jfree.ui.StandardGradientPaintTransformer;
 import org.jfree.ui.TextAnchor;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.ChartFactory;
 //import javax.swing.*;
 /**
  *
@@ -81,7 +69,7 @@ public class YoungDoubleSlit extends javax.swing.JApplet {
         wavelength =300;
         slit_width = 0;
         slit_distance= 0;        
-        bin_size = 300;
+        bin_size = 501;
         single_mode = true;
         /* Create and display the applet */
         try {
@@ -180,7 +168,7 @@ public class YoungDoubleSlit extends javax.swing.JApplet {
                 x_pos.remove(0);
                 wavelength_array.remove(0);
             }
-            return ;
+            return;
         }
         
         public void paintComponent(Graphics g)                
@@ -251,82 +239,26 @@ public class YoungDoubleSlit extends javax.swing.JApplet {
     }
 
     public JFreeChart getResultChart(){
-            DefaultCategoryDataset theAmp = new DefaultCategoryDataset();  
-            Integer radius_size = ((UpperViewPane)upperView).getRadius().size();
-            Integer wavelength_value = ((UpperViewPane)upperView).getWavelengths().get(0);
-            //Integer bin_size = bin100;
-            Double bin_width = 31.0/bin_size;
+        // XY시리즈 생성.
+        XYSeries series = new XYSeries("Histogram of light amplitude");            
+        Integer radius_size = ((UpperViewPane)upperView).getRadius().size();
+        Integer wavelength_value = ((UpperViewPane)upperView).getWavelengths().get(0);            
+        Double bin_width = 31.0/bin_size;
             
-            for( int i=0 ; i< bin_size ; i++){
-                double theta = Math.toRadians(-15.5 + bin_width*i);
-                //double alpha = Math.PI* slit_width/wavelength_value*Math.sin(theta);
-                //double beta  = Math.PI* slit_distance/wavelength_value*Math.sin(theta);
-                double alpha = Math.PI* slit_width*Math.sin(theta);
-                double beta  = Math.PI* slit_distance*Math.sin(theta);
-                double amplitude = Math.cos(beta)*Math.cos(beta)*(Math.sin(alpha)/alpha)*(Math.sin(alpha)/alpha);
-                theAmp.addValue(amplitude,"Amplitude",Double.toString(theta));
-                /**
-                System.out.println(slit_width);
-                System.out.println(slit_distance);
-                System.out.println(alpha);
-                System.out.format("alpha : "+alpha+" bin : "+theta+"  value : "+amplitude+"\n");
-                */
-            }
-            final LineAndShapeRenderer rend = new LineAndShapeRenderer();
-            final CategoryItemLabelGenerator generator = new StandardCategoryItemLabelGenerator();
-            final ItemLabelPosition p_center = new ItemLabelPosition(
-                ItemLabelAnchor.CENTER, TextAnchor.CENTER
-            );
-            final ItemLabelPosition p_below = new ItemLabelPosition(
-                ItemLabelAnchor.OUTSIDE6, TextAnchor.TOP_LEFT
-            );
-            Font f = new Font("Gulim", Font.BOLD, 14);
-            Font axisF = new Font("Gulim", Font.PLAIN, 14);
-            rend.setBaseItemLabelGenerator(generator);
-            rend.setBaseItemLabelsVisible(false);
-            rend.setBaseShapesVisible(false);
-            rend.setDrawOutlines(true);
-            rend.setUseFillPaint(true);
-            rend.setBaseFillPaint(Color.WHITE);
-            rend.setBaseItemLabelFont(f);
-            rend.setBasePositiveItemLabelPosition(p_below);
-            rend.setSeriesPaint(0,new Color(219,121,22));
-            rend.setSeriesStroke(0,new BasicStroke(
-                2.0f,
-                BasicStroke.CAP_ROUND,
-                BasicStroke.JOIN_ROUND,
-                3.0f)
-             );
-            final CategoryPlot plot = new CategoryPlot();
-            plot.setDataset(theAmp);
-            plot.setRenderer(rend);
-            plot.setOrientation(PlotOrientation.VERTICAL);             // 그래프 표시 방향
-            plot.setRangeGridlinesVisible(true);                       // X축 가이드 라인 표시여부
-            plot.setDomainGridlinesVisible(true);                      // Y축 가이드 라인 표시여부
-            // 렌더링 순서 정의 : dataset 등록 순서대로 렌더링 ( 즉, 먼저 등록한게 아래로 깔림 )
-            plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);      
-
-            // X축 세팅
-            plot.setDomainAxis(new CategoryAxis());       // X축 종류 설정
-            plot.getDomainAxis().setTickLabelFont(axisF); // X축 눈금라벨 폰트 조정
-            plot.getDomainAxis().setCategoryLabelPositions(CategoryLabelPositions.STANDARD);       // 카테고리 라벨 위치 조정
-            //plot.getDomainAxis().set
-            
-            // Y축 세팅
-            plot.setRangeAxis(new NumberAxis());                 // Y축 종류 설정
-            plot.getRangeAxis().setTickLabelFont(axisF);  // Y축 눈금라벨 폰트 조정      
-
-            // 세팅된 plot을 바탕으로 chart 생성
-            final JFreeChart chart = new JFreeChart(plot);
-
-            chart.setTitle("Amplitude of light"); // 차트 타이틀
-
-//        TextTitle copyright = new TextTitle("JFreeChart WaferMapPlot", new Font("SansSerif", Font.PLAIN, 9));
-
-//        copyright.setHorizontalAlignment(HorizontalAlignment.RIGHT);
-
-//        chart.addSubtitle(copyright);  // 차트 서브 타이틀
-          
+        for( int i=0 ; i< bin_size ; i++){
+            double theta = Math.toRadians(-15.5 + bin_width*i);
+            //double alpha = Math.PI* slit_width/wavelength_value*Math.sin(theta);
+            //double beta  = Math.PI* slit_distance/wavelength_value*Math.sin(theta);
+            double alpha = Math.PI* slit_width*Math.sin(theta);
+            double beta  = Math.PI* slit_distance*Math.sin(theta);
+            double amplitude = Math.cos(beta)*Math.cos(beta)*(Math.sin(alpha)/alpha)*(Math.sin(alpha)/alpha);                
+            // series에다가 (x,y)축 값을 추가
+            series.add( theta, amplitude);
+        }
+        // XY시리즈를 Dataset 형태로 변경
+        XYSeriesCollection data = new XYSeriesCollection(series);                  
+        final JFreeChart chart = ChartFactory.createXYLineChart("Amplitude of Light","Angle","Amp.",data,PlotOrientation.VERTICAL,true,true,false);
+        chart.setTitle("Amplitude of light"); // 차트 타이틀
         return chart;
             
     }
@@ -504,6 +436,7 @@ public class YoungDoubleSlit extends javax.swing.JApplet {
         });
 
         theDistanceSlider.setMaximum(150);
+        theDistanceSlider.setValue(0);
 
         binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, theDistanceSlits, org.jdesktop.beansbinding.ELProperty.create("${value}"), theDistanceSlider, org.jdesktop.beansbinding.BeanProperty.create("value"));
         bindingGroup.addBinding(binding);
